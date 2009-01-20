3281506c490cc053b51ddffa4de7d107409e4300
Josh Guilfoyle <jasta@devtcg.org>
2009-01-20 12:00:43 +0000

    removed my copyright header (this is romain's work, mostly)
    git-svn-id: http://five.googlecode.com/svn/trunk/android-client@1077 0f5e3bc8-7f4c-0410-bf3f-39b466953847

--- src/org/devtcg/five/music/widget/IdleListDetector.java
+++ src/org/devtcg/five/music/widget/IdleListDetector.java
@@ -1,19 +1,10 @@
 /*
  * $Id$
- *
- * Copyright (C) 2008 Josh Guilfoyle <jasta@devtcg.org>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of the GNU General Public License as published by the
- * Free Software Foundation; either version 2, or (at your option) any
- * later version.
- *
- * This program is distributed in the hope that it will be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * General Public License for more details.
+ * 
+ * Much of this logic was taken from Romain Guy's Shelves project:
+ * 
+ *   http://code.google.com/p/shelves/
  */
-
 package org.devtcg.five.music.widget;
 
 import org.devtcg.five.music.widget.FastScrollView;
