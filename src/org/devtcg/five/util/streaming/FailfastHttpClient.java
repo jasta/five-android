0042ec2ce356e10a8f3ca0c54d0b9cd79c76a878
Josh Guilfoyle <jasta00@gmail.com>
2009-11-28 09:25:37 -0800

    Forgot to add getCredentialsProvider method to FailfastHttpClient.
    This commit should have initially been amended to
    e708383f1f4a8974024ab6046cb6d245d1dfcfe6, but I forgot.

--- src/org/devtcg/five/util/streaming/FailfastHttpClient.java
+++ src/org/devtcg/five/util/streaming/FailfastHttpClient.java
@@ -16,6 +16,10 @@
 
 package org.devtcg.five.util.streaming;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.net.URI;
+
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpEntityEnclosingRequest;
@@ -24,44 +28,28 @@ import org.apache.http.HttpHost;
 import org.apache.http.HttpRequest;
 import org.apache.http.HttpRequestInterceptor;
 import org.apache.http.HttpResponse;
-import org.apache.http.entity.AbstractHttpEntity;
-import org.apache.http.entity.ByteArrayEntity;
-import org.apache.http.client.CookieStore;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.ResponseHandler;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.params.HttpClientParams;
+import org.apache.http.client.protocol.ClientContext;
 import org.apache.http.conn.ClientConnectionManager;
 import org.apache.http.conn.scheme.PlainSocketFactory;
 import org.apache.http.conn.scheme.Scheme;
 import org.apache.http.conn.scheme.SchemeRegistry;
-import org.apache.http.conn.ssl.SSLSocketFactory;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.impl.client.RequestWrapper;
-import org.apache.http.impl.conn.tsccm.ThreadSafeClientConnManager;
 import org.apache.http.params.BasicHttpParams;
 import org.apache.http.params.HttpConnectionParams;
 import org.apache.http.params.HttpParams;
 import org.apache.http.params.HttpProtocolParams;
+import org.apache.http.protocol.BasicHttpContext;
 import org.apache.http.protocol.BasicHttpProcessor;
 import org.apache.http.protocol.HttpContext;
-import org.apache.http.protocol.BasicHttpContext;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.OutputStream;
-import java.util.zip.GZIPInputStream;
-import java.util.zip.GZIPOutputStream;
-import java.net.URI;
-import java.util.concurrent.atomic.AtomicInteger;
 
 import android.util.Log;
-import android.content.ContentResolver;
-import android.provider.Settings;
-import android.text.TextUtils;
 
 /**
  * Subclass of the Apache {@link DefaultHttpClient} that is configured with
@@ -73,12 +61,12 @@ import android.text.TextUtils;
  * To retain cookies, simply add a cookie store to the HttpContext:</p>
  *
  * <pre>context.setAttribute(ClientContext.COOKIE_STORE, cookieStore);</pre>
- * 
+ *
  * <p>Uses an alternate ClientConnectionManager ultimately to work around
  * a harmony socket closure bug on Linux.</p>
  */
 public final class FailfastHttpClient implements HttpClient {
-        
+
     private static final String TAG = "FailfastHttpClient";
 
     private static final int CONNECT_TIMEOUT = 20 * 1000;
@@ -122,7 +110,7 @@ public final class FailfastHttpClient implements HttpClient {
         // Set the specified user agent and register standard protocols.
         if (userAgent != null)
         	HttpProtocolParams.setUserAgent(params, userAgent);
-        
+
         SchemeRegistry schemeRegistry = new SchemeRegistry();
         schemeRegistry.register(new Scheme("http",
                 PlainSocketFactory.getSocketFactory(), 80));
@@ -134,7 +122,7 @@ public final class FailfastHttpClient implements HttpClient {
         return new FailfastHttpClient(manager, params);
     }
 
-    private final HttpClient delegate;
+    private final DefaultHttpClient delegate;
 
     private RuntimeException mLeakedException = new IllegalStateException(
             "FailfastHttpClient created and never closed");
@@ -207,6 +195,10 @@ public final class FailfastHttpClient implements HttpClient {
         return delegate.getConnectionManager();
     }
 
+    public CredentialsProvider getCredentialsProvider() {
+        return delegate.getCredentialsProvider();
+    }
+
     public HttpResponse execute(HttpUriRequest request) throws IOException {
         return delegate.execute(request);
     }
@@ -226,7 +218,7 @@ public final class FailfastHttpClient implements HttpClient {
         return delegate.execute(target, request, context);
     }
 
-    public <T> T execute(HttpUriRequest request, 
+    public <T> T execute(HttpUriRequest request,
             ResponseHandler<? extends T> responseHandler)
             throws IOException, ClientProtocolException {
         return delegate.execute(request, responseHandler);
@@ -295,7 +287,7 @@ public final class FailfastHttpClient implements HttpClient {
         }
         if (level < Log.VERBOSE || level > Log.ASSERT) {
             throw new IllegalArgumentException("Level is out of range ["
-                + Log.VERBOSE + ".." + Log.ASSERT + "]");    
+                + Log.VERBOSE + ".." + Log.ASSERT + "]");
         }
 
         curlConfiguration = new LoggingConfiguration(name, level);
