be8a6946f8333e595c6045535ffcae2881925ded
Josh Guilfoyle <jasta@devtcg.org>
2010-04-28 22:35:53 -0700

    MemCache now prunes stale entires during each operation.

--- src/org/devtcg/five/util/MemCache.java
+++ src/org/devtcg/five/util/MemCache.java
@@ -1,15 +1,19 @@
 package org.devtcg.five.util;
 
+import java.lang.ref.ReferenceQueue;
 import java.lang.ref.SoftReference;
+import java.lang.ref.WeakReference;
 import java.util.HashMap;
 
 public class MemCache<K, V>
 {
-	private final HashMap<K, SoftReference<V>> mCache = new HashMap<K, SoftReference<V>>();
+	private final HashMap<K, CacheValue<K, V>> mCache = new HashMap<K, CacheValue<K, V>>();
+	private final ReferenceQueue<V> mRefQueue = new ReferenceQueue<V>();
 
 	public V get(K key)
 	{
-		SoftReference<V> ref = mCache.get(key);
+		prune();
+		CacheValue<K, V> ref = mCache.get(key);
 		if (ref != null)
 		{
 			V value = ref.get();
@@ -23,12 +27,14 @@ public class MemCache<K, V>
 
 	public void put(K key, V value)
 	{
-		mCache.put(key, new SoftReference<V>(value));
+		prune();
+		mCache.put(key, new CacheValue<K, V>(key, value, mRefQueue));
 	}
 
 	public V remove(K key)
 	{
-		SoftReference<V> ref = mCache.remove(key);
+		prune();
+		CacheValue<K, V> ref = mCache.remove(key);
 		if (ref != null)
 		{
 			V value = ref.get();
@@ -38,8 +44,55 @@ public class MemCache<K, V>
 		return null;
 	}
 
+	public int size()
+	{
+		return mCache.size();
+	}
+
 	public void clear()
 	{
+		while (mRefQueue.poll() != null)
+			/* Do nothing... */;
+
 		mCache.clear();
+	}
+
+	@SuppressWarnings("unchecked")
+	private void prune()
+	{
+		CacheValue<K, V> ref;
+		while ((ref = (CacheValue<K, V>)mRefQueue.poll()) != null)
+		{
+			K key = ref.key.get();
+			if (key != null)
+				mCache.remove(key);
+		}
+	}
+
+	/**
+	 * @deprecated Do not use.
+	 */
+	public ReferenceQueue<V> getReferenceQueue()
+	{
+		return mRefQueue;
+	}
+
+	private static class CacheValue<Key, Value> extends SoftReference<Value>
+	{
+		/**
+		 * Reference to the key that installed this value so we can prune
+		 * entries when the value expires.
+		 * <p>
+		 * Must be weakly referenced for the case where the entry is removed
+		 * from the cache, but still strongly referenced because of the
+		 * reference queue.
+		 */
+		private final WeakReference<Key> key;
+
+		public CacheValue(Key key, Value value, ReferenceQueue<? super Value> queue)
+		{
+			super(value, queue);
+			this.key = new WeakReference<Key>(key);
+		}
 	}
 }
